b13c59b9ddc3653f30ea949817f57ab796ac296e
Your Name <you@example.com>
2024-09-14 11:30:23 +0530

    chart small

--- rom_app/restaurant_ops_mgmt/page/inventory_view/inventory_view_sql.py
+++ rom_app/restaurant_ops_mgmt/page/inventory_view/inventory_view_sql.py
@@ -2,6 +2,29 @@ import frappe
 import yaml
 import json
 
+@frappe.whitelist()
+def inventory_transaction_by_branchwise_amount_data(filters):
+    print("inventory_transaction_by_amount_data")
+    conditions = get_conditions(filters)
+    print("-------- get data ------------")
+    print(conditions)
+    sql_purchase_order = "SELECT 'Purchase Order' AS 'inventory_transaction', SUM(purchase_order.total_price) as 'total_amount' FROM `tabPurchase Order` purchase_order"
+    sql_chef_indent = "SELECT 'Chef Indent' AS 'inventory_transaction', SUM(chef_indent.total_price) as 'total_amount' FROM `tabChef Indent By Dept` chef_indent"
+    sql_invent_wastage = "SELECT 'Inventory Wastage' AS 'inventory_transaction', SUM(invent_wastage.total_price) as 'total_amount' FROM `tabInventory Wastage` invent_wastage"
+    sql_invent_counting = "SELECT  'Inventory Counting' AS 'inventory_transaction',SUM(invent_counting.total_amount) as 'total_amount' FROM `tabInventory Counting` invent_counting"
+
+    sql_purchase_order = build_sql_where_condition(sql_purchase_order, conditions)
+    sql_chef_indent = build_sql_where_condition(sql_chef_indent, conditions)
+    sql_invent_wastage = build_sql_where_condition(sql_invent_wastage, conditions)
+    sql_invent_counting = build_sql_where_condition(sql_invent_counting, conditions)
+
+    build_sql1 = f"  {sql_purchase_order} UNION  {sql_chef_indent} "
+    build_sql2 = f" UNION  {sql_invent_wastage} UNION {sql_invent_counting} "
+
+    build_sql = build_sql1 + build_sql2
+    print(build_sql)
+    data = execute_sql(build_sql)
+    return data
 
 @frappe.whitelist()
 def inventory_transaction_by_amount_data(filters):
